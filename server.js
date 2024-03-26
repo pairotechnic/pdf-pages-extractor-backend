9b6d65b55d366ff2e809d22a7fb0a685b81e239f
Rohit Pai <rohitkalsankpai@gmail.com>
2024-03-26 14:36:23 +0530

    Stable : Local Frontend, Local Backend, AWS

--- server.js
+++ server.js
@@ -5,41 +5,37 @@ const cors = require('cors') // for enabling Cross-Origin Resource Sharing
 const { PDFDocument } = require('pdf-lib'); // for PDF manipulation
 const fs = require('fs'); // for file system operations
 const dotenv = require('dotenv') // for using .env file
+const AWS = require('aws-sdk')
+const { v4 : uuidv4 } = require('uuid')
 
 dotenv.config() // loads contents of .env into process.env
 // Only required during development, because in production we directly enter the environment variables in VERCEL 
 
+// Configure AWS SDK
+AWS.config.update({
+  accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
+  region : process.env.AWS_REGION
+})
+
+const s3 = new AWS.S3()
 
 const app = express() // Express application is created
 
 app.use(cors()) // used to allow cross-origin requests
 app.use(express.json()); // used to parse incoming JSON payloads
 
-// Static file serving is configured for the folders 'uploads' and 'generated'
-// This allows the frontend to access the uploaded and generated files PDF files
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/generated', express.static(path.join(__dirname, 'generated')));
-
-//Configure Multer Storage
-const storage = multer.diskStorage({ // custom storage engine
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/') // saves uploaded files to the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname) // uses the original filename
-  }
-})
-
+// Use multer.memoryStorage for storing files in memory
 const upload = multer({
-  storage: storage,
+  storage: multer.memoryStorage(),
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'application/pdf') { // file filter only accepts PDFs
-      cb(null, true)
-    } else {
-      cb(new Error('Only PDFs are allowed!'), false)
-    }
+     if (file.mimetype === 'application/pdf') {
+       cb(null, true);
+     } else {
+       cb(new Error('Only PDFs are allowed!'), false);
+     }
   }
-})
+ });
 
 // Backend testing route
 app.get('/superman', (req, res) => {
@@ -47,58 +43,88 @@ app.get('/superman', (req, res) => {
 })
 
 // File Upload route
-app.post('/api/upload', upload.single('pdf'), (req, res) => { // accepts a single file upload, with the field name 'pdf'
-  const filePath = path.join('uploads', req.file.filename) // saves file to the 'uploads' directory
-  res.status(200).json({ message: 'File Uploaded Successfully', filePath })
-})
+app.post('/api/upload', upload.single('pdf'), async (req, res) => { // accepts a single file upload, with the field name 'pdf'
 
-// PDF Download route
-app.get('/api/pdf/:filename', (req, res) => { 
-  const filePath = path.join('uploads', req.params.filename)
-  res.sendFile(filePath) // allows downloading a PDF file from the uploads directory by its filename
+  try{
+    const file = req.file
+    // const fileName = `${uuidv4()}-${file.originalname}`; // Generate a unique file name
+    const fileName = `${file.originalname}-${uuidv4()}`; // Generate a unique file name
+
+    const params = {
+      Bucket : 'pairotechnic-aws-bucket',
+      // Key : `uploads/${file.originalname}`, // File will be saved in the 'uploads' folder in S3
+      Key: `uploads/${fileName}`, // File will be saved in the 'uploads' folder in S3
+      Body: file.buffer,
+      ACL : 'public-read' // Make the file publicly accessible
+    }
+
+    const response = await s3.upload(params).promise()
+    const filePath = response.Location; // This is the URL of the uploaded file in S3
+
+    console.log("filePath : " , filePath)
+
+    res.status(200).json({ message : 'File Uploaded Successfully', filePath})
+
+  } catch (error) {
+    console.error('Error uploading file to S3 : ', error)
+    res.status(500).json({ message : 'File Upload Failed', error : error.message})
+  }
+
 })
 
 // Route for generating new pdf from extracted pages
 app.post('/api/extract-pages', async (req, res) => {
+
   try {
+
     const { originalPdfPath, selectedPages } = req.body;
 
     // Sort selectedPages in ascending order
     selectedPages.sort((a, b) => a - b);
 
-    const originalPdfBytes = fs.readFileSync(path.join(__dirname, originalPdfPath));
+    // Fetch the PDF from S3 into memory
+    const params = {
+      Bucket: 'pairotechnic-aws-bucket',
+      Key: originalPdfPath,
+    };
+    const data = await s3.getObject(params).promise();
+    const originalPdfBytes = data.Body; // This is a Buffer
+
+    // Load the PDF from the Buffer
     const originalPdfDoc = await PDFDocument.load(originalPdfBytes);
-    const generatedPdfDoc = await PDFDocument.create();
+    const generatedPdfDoc = await PDFDocument.create()
 
-    // Ensure the "generated" folder exists
-    const generatedFolderPath = path.join(__dirname, 'generated');
-    if (!fs.existsSync(generatedFolderPath)) {
-      fs.mkdirSync(generatedFolderPath);
-    }
+    // Extract the original filename without the path, and adding datetime to make it unique
 
-    // Extract the original filename without the path
-    const originalFilename = path.basename(originalPdfPath)
-    // Remove the extension from the original filename
-    const originalName = originalFilename.split('.')[0]
-    // Generate a unique name for the new PDF by appending a timestamp
+    console.log("originalPdfPath : " , originalPdfPath)
+    const originalFilename = path.basename(originalPdfPath);
+    console.log("originalFilename : " , originalFilename)
+    const originalName = originalFilename.split('.')[0];
+    console.log("originalName : " , originalName)
     const generatedPdfName = `${originalName}-${Date.now()}.pdf`;
+    console.log("generatedPdfName : " , generatedPdfName)
 
-
-    // Use Promise.all to wait for all page copying operations to complete
+    // Extract the selected pages
     await Promise.all(selectedPages.map(async (pageNumber) => {
       const [copiedPage] = await generatedPdfDoc.copyPages(originalPdfDoc, [pageNumber - 1]);
-      generatedPdfDoc.addPage(copiedPage);
+      generatedPdfDoc.addPage(copiedPage)
     }));
 
+    // Save the generated PDF to a Buffer
     const generatedPdfBytes = await generatedPdfDoc.save();
 
-    // const generatedPdfPath = path.join(generatedFolderPath, `new-${Date.now()}.pdf`);
-    // fs.writeFileSync(generatedPdfPath, generatedPdfBytes);
+    // Upload the generated PDF to S3
+    const uploadParams = {
+      Bucket: 'pairotechnic-aws-bucket',
+      Key: `generated/${generatedPdfName}`,
+      Body: generatedPdfBytes,
+      ACL: 'public-read',
+    };
+    const response = await s3.upload(uploadParams).promise();
+    const filePath = response.Location;
 
-    const generatedPdfPath = `generated/${generatedPdfName}`;
-    fs.writeFileSync(path.join(__dirname, generatedPdfPath), generatedPdfBytes);
+    res.status(200).json({ message: 'New PDF created successfully', filePath });
 
-    res.status(200).json({ message: 'New PDF Created Successfully', filePath: generatedPdfPath });
   } catch (error) {
     console.error('Error creating new PDF:', error);
     res.status(500).json({ message: 'Error creating new PDF', error: error.message });
