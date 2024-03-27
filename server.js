54c8393cc3f7123982f70aa46314948e0f363b1d
Rohit Pai <rohitkalsankpai@gmail.com>
2024-03-27 19:37:59 +0530

    Spaces in filename are handled properly

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv') // for using .env file
 const AWS = require('aws-sdk')
 const { v4 : uuidv4 } = require('uuid')
 
-dotenv.config() // loads contents of .env into process.env
+// dotenv.config() // loads contents of .env into process.env
 // Only required during development, because in production we directly enter the environment variables in VERCEL 
 
 // Configure AWS SDK
@@ -45,26 +45,22 @@ app.get('/superman', (req, res) => {
 // File Upload route
 app.post('/api/upload', upload.single('pdf'), async (req, res) => { // accepts a single file upload, with the field name 'pdf'
 
+  console.log("\tUPLOAD ")
+
   try{
     const file = req.file
-    // const fileName = `${uuidv4()}-${file.originalname}`; // Generate a unique file name
-    const fileName = `${file.originalname}-${uuidv4()}`; // Generate a unique file name
-
+    const originalFileName = `${file.originalname}` 
+    const originalBaseName = originalFileName.split('.')[0]
+    const uniqueFileName = `${originalBaseName}-${uuidv4()}.pdf`; 
     const params = {
-      Bucket : 'pairotechnic-aws-bucket',
-      // Key : `uploads/${file.originalname}`, // File will be saved in the 'uploads' folder in S3
-      Key: `uploads/${fileName}`, // File will be saved in the 'uploads' folder in S3
+      Bucket : process.env.AWS_BUCKET_NAME,
+      Key: `uploads/${uniqueFileName}`, // File will be saved in the 'uploads' folder in S3
       Body: file.buffer,
       ACL : 'public-read' // Make the file publicly accessible
     }
-
     const response = await s3.upload(params).promise()
     const filePath = response.Location; // This is the URL of the uploaded file in S3
-
-    console.log("filePath : " , filePath)
-
     res.status(200).json({ message : 'File Uploaded Successfully', filePath})
-
   } catch (error) {
     console.error('Error uploading file to S3 : ', error)
     res.status(500).json({ message : 'File Upload Failed', error : error.message})
@@ -74,63 +70,48 @@ app.post('/api/upload', upload.single('pdf'), async (req, res) => { // accepts a
 
 // Route for generating new pdf from extracted pages
 app.post('/api/extract-pages', async (req, res) => {
-
   try {
-
     const { originalPdfPath, selectedPages } = req.body;
-
+    // Decode the originalPdfPath to handle spaces correctly
+    const decodedOriginalPdfPath = decodeURIComponent(originalPdfPath);
     // Sort selectedPages in ascending order
     selectedPages.sort((a, b) => a - b);
-
     // Fetch the PDF from S3 into memory
     const params = {
-      Bucket: 'pairotechnic-aws-bucket',
-      Key: originalPdfPath,
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: decodedOriginalPdfPath,
     };
     const data = await s3.getObject(params).promise();
     const originalPdfBytes = data.Body; // This is a Buffer
-
     // Load the PDF from the Buffer
     const originalPdfDoc = await PDFDocument.load(originalPdfBytes);
     const generatedPdfDoc = await PDFDocument.create()
-
-    // Extract the original filename without the path, and adding datetime to make it unique
-
-    console.log("originalPdfPath : " , originalPdfPath)
-    const originalFilename = path.basename(originalPdfPath);
-    console.log("originalFilename : " , originalFilename)
-    const originalName = originalFilename.split('.')[0];
-    console.log("originalName : " , originalName)
+    const originalFilename = path.basename(decodedOriginalPdfPath);
+    const originalName = originalFilename.substring(0, originalFilename.length - 41); // 41 characters inclueds the hyphen, the unique string, the .pdf
     const generatedPdfName = `${originalName}-${Date.now()}.pdf`;
-    console.log("generatedPdfName : " , generatedPdfName)
-
     // Extract the selected pages
     await Promise.all(selectedPages.map(async (pageNumber) => {
       const [copiedPage] = await generatedPdfDoc.copyPages(originalPdfDoc, [pageNumber - 1]);
       generatedPdfDoc.addPage(copiedPage)
     }));
-
     // Save the generated PDF to a Buffer
     const generatedPdfBytes = await generatedPdfDoc.save();
-
     // Upload the generated PDF to S3
     const uploadParams = {
-      Bucket: 'pairotechnic-aws-bucket',
+      Bucket: process.env.AWS_BUCKET_NAME,
       Key: `generated/${generatedPdfName}`,
       Body: generatedPdfBytes,
       ACL: 'public-read',
     };
     const response = await s3.upload(uploadParams).promise();
     const filePath = response.Location;
-
     res.status(200).json({ message: 'New PDF created successfully', filePath });
-
   } catch (error) {
     console.error('Error creating new PDF:', error);
     res.status(500).json({ message: 'Error creating new PDF', error: error.message });
   }
 });
 
-app.listen(process.env.port, () => {
-  console.log(`Server running at http://localhost:${process.env.port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`)
 })
